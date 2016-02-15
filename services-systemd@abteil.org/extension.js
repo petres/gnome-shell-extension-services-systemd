61be45e1afa3d14f436bce04208eec5a5e07d336
petres <peter.reschenhofer@gmail.com>
2016-02-15 17:13:18 +0100

    restructering extension

--- services-systemd@abteil.org/extension.js
+++ services-systemd@abteil.org/extension.js
@@ -9,79 +9,79 @@ const Me = ExtensionUtils.getCurrentExtension();
 const Convenience = Me.imports.convenience;
 const Util = imports.misc.util;
 
-function Services() {
-    this._settings = Convenience.getSettings();
-    this._settings.connect('changed', Lang.bind(this, this.loadConfig));
-    this.loadConfig();
+const ServicesManager = new Lang.Class({
+    Name: 'ServicesManager',
+    Extends: PanelMenu.Button,
 
-	this.button = new PanelMenu.Button(0.0);
-	
-	let hbox = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
-	let icon = new St.Icon({icon_name: 'system-run-symbolic', style_class: 'system-status-icon'});
-	hbox.add_child(icon);
+    _entries: [],
 
-	this.button.actor.add_actor(hbox);
-	this.button.actor.add_style_class_name('panel-status-button');
+	_init: function() {
+		PanelMenu.Button.prototype._init.call(this, 0.0);
+		
+	    this._settings = Convenience.getSettings();
+	    this._settings.connect('changed', Lang.bind(this, this._loadConfig));
 
-	this.button.actor.connect('button-press-event', Lang.bind(this, function() {
-		this.refresh();
-	}));
-	Main.panel.addToStatusArea('services', this.button);
-	this.refresh();
-}
+	    this._loadConfig();
 
-Services.prototype = {
-	refresh: function() {
-		this.button.menu.removeAll();
+		let hbox = new St.BoxLayout({ style_class: 'panel-status-menu-box' });
+		let icon = new St.Icon({icon_name: 'system-run-symbolic', style_class: 'system-status-icon'});
+		hbox.add_child(icon);
+
+		this.actor.add_actor(hbox);
+		this.actor.add_style_class_name('panel-status-button');
+
+		this.actor.connect('button-press-event', Lang.bind(this, function() {
+			this._refresh();
+		}));
+
+		Main.panel.addToStatusArea('servicesManager', this);
+		
+		this._refresh();
+	},
+
+	_refresh: function() {
+		this.menu.removeAll();
 		this._entries.forEach(Lang.bind(this, function(service) {
 			let active = false;
 			let [_, out, err, stat] = GLib.spawn_command_line_sync('systemctl is-active ' + service['service']);
 			let active = (stat == 0);
 			let item = new PopupMenu.PopupSwitchMenuItem(service['name'], active);
-			this.button.menu.addMenuItem(item);
+			this.menu.addMenuItem(item);
 			item.connect('toggled', function() {
 				switch(service["type"]) {
 					case 'systemd':
 						GLib.spawn_command_line_async('sh -c "pkexec --user root systemctl ' + (active ? 'stop' : 'start') + ' ' + service['service'] + '; exit;"');
-						//GLib.spawn_command_line_async('sh -c "gksudo systemctl ' + (active ? 'stop' : 'start') + ' ' + service['service'] + '; exit;"');
 				}
 			});
 		}));
 		if(this._entries.length > 0)
-	        this.button.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+	        this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
         let item = new PopupMenu.PopupMenuItem(_("Add systemd services ..."));
         item.connect('activate', Lang.bind(this, function() {
 	        Util.spawn(["gnome-shell-extension-prefs", "services-systemd@abteil.org"]);
-	        this.button.menu.close();
+	        this.menu.close();
 	    }));
-        this.button.menu.addMenuItem(item);
+        this.menu.addMenuItem(item);
         return true;
 	},
 
-    loadConfig: function() {
+    _loadConfig: function() {
         let entries = this._settings.get_strv("systemd");
-        this._entries = [ ]
+        this._entries = []
         for (let i = 0; i < entries.length; i++) {
 	        let entry = JSON.parse(entries[i]);
 	        entry["type"] = "systemd"
 	        this._entries.push(entry);
 	    }
-    },
+    }
+});
 
-	destroy: function() {
-		this.button.destroy();
-	}
-};
-
-function init() {
-}
-
-var services;
+let serviceManager;
 
 function enable() {
-	services = new Services();
+	serviceManager = new ServicesManager();
 }
 
 function disable() {
-	services.destroy();
+	serviceManager.destroy();
 }
