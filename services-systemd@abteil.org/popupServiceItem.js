1f13078241e3a4f17b6edf3be41f21a7af9ffa9f
petres <peter.reschenhofer@gmail.com>
2020-04-29 09:16:37 +0200

    Adaption to new gnome shell (remove warning)

--- services-systemd@abteil.org/popupServiceItem.js
+++ services-systemd@abteil.org/popupServiceItem.js
@@ -1,28 +1,28 @@
-const Lang = imports.lang;
 const PopupMenu = imports.ui.popupMenu;
-const Util = imports.misc.util;
 const { GObject, Gtk, St, Clutter} = imports.gi;
 
-const ExtensionSystem = imports.ui.extensionSystem;
-const ExtensionUtils = imports.misc.extensionUtils;
-
 var PopupServiceItem = GObject.registerClass(
-class PopupServiceItem extends PopupMenu.PopupSwitchMenuItem {
-    _init(text, active, params) {
-        super._init(text, active);
+    class PopupServiceItem extends PopupMenu.PopupSwitchMenuItem {
+        _init(text, active, params) {
+            super._init(text, active);
 
-        if (params.restartButton) {
-            this.restartButton = new St.Button({
-                x_align: 1,
-                reactive: true,
-                can_focus: true,
-                track_hover: true,
-                accessible_name: 'restart',
-                style_class: 'system-menu-action services-systemd-button-reload'
-            });
+            if (params.restartButton) {
+                this.restartButton = new St.Button({
+                    x_expand: false,
+                    x_align: Gtk.Align.END,
+                    reactive: true,
+                    can_focus: true,
+                    track_hover: true,
+                    accessible_name: 'restart',
+                    style_class: 'system-menu-action services-systemd-button-reload'
+                });
 
-            this.restartButton.child = new St.Icon({ icon_name: 'view-refresh-symbolic' });
-            this.add(this.restartButton, { expand: false, x_align: St.Align.END });
+                this.restartButton.add_actor(new St.Icon({
+                    icon_name: 'view-refresh-symbolic',
+                }));
+
+                this.add_child(this.restartButton);
+            }
         }
     }
-});
+);
